c6415f2501339cbed1067b89cf54ae3440f4a719
dEVtyagi <devtyagi703@gmail.com>
2024-01-19 17:21:53 +0530

    Add cpp code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CODSOFT
 This is my CODSOFT git repository
+<br>
+It contains Task 1, Task 2, Task3.
+<br>
+# Task 1 - Number Guessing Game using c++
+<br>
+# Task 2 - Simple Calculator using  c++
+<br>
+# Task 3 - TIC-TAC-TOE game using C++
--- Task 2/SimpleCalculator.cpp
+++ Task 2/SimpleCalculator.cpp
@@ -1,2 +1,48 @@
 #include<iostream>
 using namespace std;
+int main(){
+
+    float numOne, numTwo, result;
+    int choice;
+    do{
+        cout << "1. Addition\n";
+        cout << "2. Subtraction\n";
+        cout << "3. Multiplication\n";
+        cout << "4. Division\n";
+        cout << "5. Exit\n\n";
+        cout << "Enter Your Choice(1-5): ";
+        cin >> choice ;
+        if( choice >= 1 && choice <=4 )
+        {
+            cout << "\nEnter any two Numbers: " ;
+            cin >> numOne >> numTwo ;
+        }
+        switch(choice)
+        {
+            case 1:
+                result = numOne+numTwo;
+                cout<<"\nResult = "<<result;
+                break;
+            case 2:
+                result = numOne-numTwo;
+                cout<<"\nResult = "<<result;
+                break;
+            case 3:
+                result = numOne*numTwo;
+                cout<<"\nResult = "<<result;
+                break;
+            case 4:
+                result = numOne/numTwo;
+                cout<<"\nResult = "<<result;
+                break;
+            case 5:
+                return 0;
+            default:
+                cout<<"\nWrong Choice!";
+                break;
+        }
+        cout<<"\n------------------------\n";
+    }while(choice!=5);
+    cout<<endl;
+    return 0;
+}
